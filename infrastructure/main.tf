874b5bf469d7fce28ff2ecaa936ac6072a57aa8d
Nathan DeMaria <nathandmaria@gmail.com>
2021-02-15 21:05:56 -0600

    Add an "allow ssh" flag

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,20 +37,38 @@ data "aws_ami" "ecs_ami" {
 resource "aws_security_group" "sg" {
   name        = "new-years-day"
   description = "Controls access to the API"
+}
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = local.app_port
-    to_port     = local.app_port
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "app" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = local.app_port
+  to_port           = local.app_port
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+}
 
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  count             = var.allow_ssh ? 1 : 0
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
+  security_group_id = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
 }
 
 resource "aws_iam_instance_profile" "server" {
---
+++ infrastructure/variables.tf
@@ -0,0 +1,5 @@
+variable "allow_ssh" {
+  description = "If true, will open up the ssh port on the instance"
+  default     = false
+  type        = bool
+}
